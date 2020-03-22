1cfe7968475128f9b520c969b9994f2afad75d63
MinesJA <minesja@gmail.com>
2020-03-22 12:56:41 -0400

    Creaated Spring Rest Service

---
+++ src/main/java/com/minesja/poker/pokerrestservice/PokerRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.minesja.poker.pokerrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minesja/poker/pokerrestservice/PokerRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minesja.poker.pokerrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
