bd7d4f53eaa11a19c34a4522b95fca9c99e4270d
MinesJA <minesja@gmail.com>
2020-04-21 14:16:47 -0400

    Added frame for cli

--- src/main/java/com/minesja/poker/pokerrestservice/CLI.java
+++ src/main/java/com/minesja/poker/pokerrestservice/CLI.java
@@ -3,7 +3,10 @@ package com.minesja.poker.pokerrestservice;
 import picocli.CommandLine;
 
 @CommandLine.Command(
-    
+    name = "Hello",
+    description = "Says hello"
 )
 public class CLI {
+
+    
 }
